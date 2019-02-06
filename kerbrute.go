5135724938063647ad1cf7650b808369fde91354
Ronnie Flathers <rflathers@gmail.com>
2019-02-05 21:16:19 -0600

    moved logging to main

--- kerbrute.go
+++ kerbrute.go
@@ -12,10 +12,17 @@ import (
 var opts struct {
 	Domain           string `short:"d" long:"domain" required:"true" name:"domain name" description:"Full name of domain (e.g. contoso.com). Required."`
 	DomainController string `long:"dc" required:"false" name:"Domain Controller (KDC)" description:"DC to target. If not supplied, will attempt to find through DNS"`
+	Verbose          bool   `short:"v" long:"verbose" description:"Show all failed attempts too"`
 	Args             struct {
 		UsernameList string `positional-arg-name:"<username_list>"`
 		Password     string `positional-arg-name:"<password_to_try>"`
 	} `positional-args:"yes" required:"yes"`
+}
+
+func handleKerbError(err error) {
+	if opts.Verbose {
+		log.Printf(err.Error())
+	}
 }
 
 func main() {
@@ -35,22 +42,25 @@ func main() {
 		log.Printf("\t%s", v)
 	}
 
-	kSession.testLogin("agreen", "foobar")
-
 	file, err := os.Open(opts.Args.UsernameList)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+	log.Printf("Testing usernames from %q", file.Name())
 
 	scanner := bufio.NewScanner(file)
 	var username string
 	for scanner.Scan() {
 		username = scanner.Text()
+		if opts.Verbose {
+			log.Printf("Testing Login: \t%v@%v : %v", username, domain, opts.Args.Password)
+		}
+
 		if ok, err := kSession.testLogin(username, opts.Args.Password); ok {
-			log.Printf("[!] Valid Login: \t%v : %v", username, opts.Args.Password)
+			log.Printf("[+] Sucess! \t%v@%v : %v", username, domain, opts.Args.Password)
 		} else {
-			kSession.handleKerbError(err)
+			handleKerbError(err)
 		}
 	}
 	log.Println("...done!")
