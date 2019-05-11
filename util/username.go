9f5cfa310d7d27b8ba0e05d8cf424cdfda2ac4bb
Ronnie Flathers <rflathers@gmail.com>
2019-05-11 18:08:58 -0500

    read combos from file and bruteforce working

--- util/username.go
+++ util/username.go
@@ -15,3 +15,22 @@ func FormatUsername(username string) (user string, err error) {
 	}
 	return parts[0], nil
 }
+
+func FormatComboLine(combo string) (username string, password string, err error) {
+	parts := strings.SplitN(combo, ":", 2)
+	if len(parts) == 0 {
+		err = errors.New("Bad format - missing ':'")
+		return "", "", err
+	}
+	user, err := FormatUsername(parts[0])
+	if err != nil {
+		return "", "", err
+	}
+	pass := strings.Join(parts[1:], "")
+	if pass == "" {
+		err = errors.New("Password is blank")
+		return "", "", err
+	}
+	return user, pass, err
+
+}
