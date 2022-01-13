a371baaf9ad325a0b918a98942c51a6604d045cf
deadjakk <deadjakk@shell.rip>
2022-01-13 09:57:03 -0600

    removed old comments

--- util/file.go
+++ util/file.go
@@ -39,38 +39,3 @@ func is_present(arr []string, val string) bool {
   }
   return false
 }
-
-//type Logger struct {
-	//Log *logging.Logger
-//}
-
-//func NewLogger(verbose bool, logFileName string) Logger {
-	//log := logging.MustGetLogger("kerbrute")
-	//format := logging.MustStringFormatter(
-		//`%{color}%{time:2006/01/02 15:04:05} >  %{message}%{color:reset}`,
-	//)
-	//formatNoColor := logging.MustStringFormatter(
-		//`%{time:2006/01/02 15:04:05} >  %{message}`,
-	//)
-	//backend := logging.NewLogBackend(os.Stdout, "", 0)
-	//backendFormatter := logging.NewBackendFormatter(backend, format)
-
-	//if logFileName != "" {
-		//outputFile, err := os.Create(logFileName)
-		//if err != nil {
-			//panic(err)
-		//}
-		//fileBackend := logging.NewLogBackend(outputFile, "", 0)
-		//fileFormatter := logging.NewBackendFormatter(fileBackend, formatNoColor)
-		//logging.SetBackend(backendFormatter, fileFormatter)
-	//} else {
-		//logging.SetBackend(backendFormatter)
-	//}
-
-	//if verbose {
-		//logging.SetLevel(logging.DEBUG, "")
-	//} else {
-		//logging.SetLevel(logging.INFO, "")
-	//}
-	//return Logger{log}
-//}
